ab0567e6093467f1ee9381ecdd6ea692ff8e81d1
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-10 20:07:30 +0600

    services complete

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import fluoride from "../../../assets/images/fluoride.png";
+import cavity from "../../../assets/images/cavity.png";
+import whitening from "../../../assets/images/whitening.png";
+import ServiceCard from '../ServiceCard/ServiceCard';
+
+const Services = () => {
+
+    const services = [
+        {
+            _id: 1,
+            name: "Fluoride Treatment",
+            description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+            img: fluoride
+        },
+        {
+            _id: 2,
+            name: "Cavity Filling",
+            description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+            img: cavity
+        },
+        {
+            _id: 3,
+            name: "Teeth Whitening",
+            description: "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+            img: whitening
+        }
+    ]
+
+
+
+    return (
+        <div className='my-24'>
+            <div className='text-center'>
+                <h3 className='text-primary text-xl font-bold uppercase'>Our Services</h3>
+                <h2 className='text-4xl'>Service We Provide</h2>
+            </div>
+            <div className='w-4/5 mx-auto py-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
